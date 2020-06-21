d38f322b11c01dc1400eaab6d1d14bed94a08ce9
Michael Qiu <michael.yiming.qiu@gmail.com>
2020-06-21 13:46:16 +1000

    LeetCode 142

--- src/Algorithm/LeetCode/LeetCode_142_LinkedListCycleII/LeetCode_141_LinkedListCycle/Solution_HashSet.java
+++ src/Algorithm/LeetCode/LeetCode_142_LinkedListCycleII/LeetCode_141_LinkedListCycle/Solution_HashSet.java
@@ -1,29 +1,29 @@
-package Algorithm.LeetCode.LeetCode_141_LinkedListCycle;
+package Algorithm.LeetCode.LeetCode_142_LinkedListCycleII.LeetCode_141_LinkedListCycle;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class Solution_HashSet {
-    public boolean hasCycle(ListNode head) {
+    public ListNode detectCycle(ListNode head) {
         Set<ListNode> nodeSet = new HashSet<>();
 
         while (head != null) {
             if (nodeSet.contains(head)) {
-                return true;
+                return head;
             }
 
             nodeSet.add(head);
             head = head.next;
         }
 
-        return false;
+        return null;
     }
 
     public static void main(String[] args) {
         Solution_HashSet foo = new Solution_HashSet();
 
         ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        System.out.println(foo.hasCycle(head));
+        System.out.println(foo.detectCycle(head));
 
         ListNode head4 = new ListNode(-4);
         ListNode head3 = new ListNode(0);
@@ -33,6 +33,6 @@ public class Solution_HashSet {
         head3.next = head4;
         head4.next = head2;
 
-        System.out.println(foo.hasCycle(head1));
+        System.out.println(foo.detectCycle(head1));
     }
 }
