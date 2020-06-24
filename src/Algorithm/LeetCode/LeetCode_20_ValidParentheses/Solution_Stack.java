8fc7dcd912bb8ce6fa879fe95cdd5a9f5050b055
Michael Qiu <michael.yiming.qiu@gmail.com>
2020-06-24 23:27:44 +1000

    LeetCode 20

---
+++ src/Algorithm/LeetCode/LeetCode_20_ValidParentheses/Solution_Stack.java
@@ -0,0 +1,40 @@
+package Algorithm.LeetCode.LeetCode_20_ValidParentheses;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution_Stack {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        Map<Character, Character> map = new HashMap<>();
+
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty() && stack.peek() == map.get(s.charAt(i))) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution_Stack foo = new Solution_Stack();
+
+        System.out.println(foo.isValid("()"));
+        System.out.println(foo.isValid("()[]{}"));
+        System.out.println(foo.isValid("(]"));
+        System.out.println(foo.isValid("([)]"));
+        System.out.println(foo.isValid("{[]}"));
+    }
+}
