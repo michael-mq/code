871cdca96d8ae51399caca91a567faa1acadf6b8
Michael Qiu <michael.yiming.qiu@gmail.com>
2020-06-30 12:36:36 +1000

    LeetCode 236

---
+++ src/Algorithm/LeetCode/LeetCode_236_Lowest_Common_Ancestor_of_a_Binary_Tree/TreeNode.java
@@ -0,0 +1,17 @@
+package Algorithm.LeetCode.LeetCode_236_Lowest_Common_Ancestor_of_a_Binary_Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this(val, null, null);
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
