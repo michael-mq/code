d03c1472cbcdffb5f2eae4933f79a3eb50aa1068
Michael Qiu <michael.yiming.qiu@gmail.com>
2020-08-29 13:17:19 +1000

    509

---
+++ src/Algorithm/LeetCode/LeetCode_509_Fibonacci_Number/Solution.java
@@ -0,0 +1,26 @@
+package Algorithm.LeetCode.LeetCode_509_Fibonacci_Number;
+
+public class Solution {
+    public int fib(int N) {
+        if (N < 2) return N;
+
+        int[] f = new int[N + 1];
+
+        f[0] = 0;
+        f[1] = 1;
+
+        for (int i = 2; i < N + 1; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+
+        return f[N];
+    }
+
+    public static void main(String[] args) {
+        Solution foo = new Solution();
+
+        System.out.println(foo.fib(2));
+        System.out.println(foo.fib(3));
+        System.out.println(foo.fib(4));
+    }
+}
