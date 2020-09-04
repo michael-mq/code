61f94576a7e63b1bbf1677570303760c88fe4be7
Michael Qiu <michael.yiming.qiu@gmail.com>
2020-09-05 01:01:07 +1000

    121

---
+++ src/Algorithm/LeetCode/LeetCode_121_Best_Time_to_Buy_and_Sell_Stock/Solution.java
@@ -0,0 +1,25 @@
+package Algorithm.LeetCode.LeetCode_121_Best_Time_to_Buy_and_Sell_Stock;
+
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(solution.maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
