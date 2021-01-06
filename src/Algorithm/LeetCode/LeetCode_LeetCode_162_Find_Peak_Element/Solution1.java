71e18c9700b2e5c23a4c7e18a6406f20e4c950b4
Michael Qiu <michael.yiming.qiu@gmail.com>
2021-01-06 16:45:15 +1100

    162

---
+++ src/Algorithm/LeetCode/LeetCode_LeetCode_162_Find_Peak_Element/Solution1.java
@@ -0,0 +1,13 @@
+package Algorithm.LeetCode.LeetCode_LeetCode_162_Find_Peak_Element;
+
+public class Solution1 {
+    public int findPeakElement(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+
+        return nums.length - 1;
+    }
+}
