35fbedd39ac3b23a4f367a8ff574081b38d03da9
Michael Qiu <michael.yiming.qiu@gmail.com>
2021-01-30 23:10:57 +1100

    138

---
+++ src/Algorithm/LeetCode/LeetCode_138_Copy_List_with_Random_Pointer/Node.java
@@ -0,0 +1,13 @@
+package Algorithm.LeetCode.LeetCode_138_Copy_List_with_Random_Pointer;
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
---
+++ src/Algorithm/LeetCode/LeetCode_138_Copy_List_with_Random_Pointer/Solution.java
@@ -0,0 +1,45 @@
+package Algorithm.LeetCode.LeetCode_138_Copy_List_with_Random_Pointer;
+
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null)
+            return null;
+
+        Map<Node, Node> map = new HashMap<>();
+
+        Node cur = head;
+
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+
+        return map.get(head);
+    }
+}
