9f8c107dd20b2a9891bacd1714fc0af4a98ed2aa
Michael Qiu <michael.yiming.qiu@gmail.com>
2021-02-10 00:34:54 +1100

    283

--- src/Algorithm/LeetCode/LeetCode_283_Move_Zeroes/Solution2.java
+++ src/Algorithm/LeetCode/LeetCode_283_Move_Zeroes/Solution2.java
@@ -2,28 +2,28 @@ package Algorithm.LeetCode.LeetCode_283_Move_Zeroes;
 
 import java.util.Arrays;
 
-public class Solution {
+public class Solution2 {
     public void moveZeroes(int[] nums) {
         if (nums == null || nums.length <= 1) {
             return;
         }
 
-        //第一次遍历的时候，j指针记录非0的个数，只要是非0的统统都赋给nums[j]
+        //两个指针i和j
         int j = 0;
-        for (int i = 0; i < nums.length; ++i) {
+
+        for (int i = 0; i < nums.length; i++) {
+            //当前元素!=0，就把其交换到左边，等于0的交换到右边
             if (nums[i] != 0) {
-                nums[j++] = nums[i];
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
             }
-        }
-        //非0元素统计完了，剩下的都是0了
-        //所以第二次遍历把末尾的元素都赋为0即可
-        for (int i = j; i < nums.length; ++i) {
-            nums[i] = 0;
         }
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution2 solution = new Solution2();
 
         int[] foo = new int[]{0, 1, 0, 3, 12};
 
