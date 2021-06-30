c871df1732240ed8aa7ea9941eaa86c22d5f1114
Michael Qiu <michael.yiming.qiu@gmail.com>
2021-06-30 21:42:34 +1000

    46

---
+++ src/Algorithm/LeetCode/LeetCode_46_Permutations/Solution.java
@@ -0,0 +1,44 @@
+package Algorithm.LeetCode.LeetCode_46_Permutations;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+
+        if (len == 0) {
+            return res;
+        }
+
+        boolean[] used = new boolean[len];
+        Deque<Integer> path = new ArrayDeque<>();
+        int depth = 0;
+
+        dfs(nums, res, used, path, depth);
+
+        return res;
+    }
+
+    private void dfs(int[] nums, List<List<Integer>> res, boolean[] used, Deque<Integer> path, int depth) {
+        if (depth == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                path.addLast(nums[i]);
+
+                dfs(nums, res, used, path, depth + 1);
+
+                used[i] = false;
+                path.removeLast();
+            }
+        }
+    }
+}
