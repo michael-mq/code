5f685bfd3f96866df03ba1ddc40931b5529c6816
Michael Qiu <michael.yiming.qiu@gmail.com>
2021-07-24 22:36:04 +1000

    76

---
+++ src/Algorithm/LeetCode/LeetCode_3_Longest_Substring_WIthout_Repeating_Characters/SlidingWindow2.java
@@ -0,0 +1,34 @@
+package Algorithm.LeetCode.LeetCode_3_Longest_Substring_WIthout_Repeating_Characters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SlidingWindow2 {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> window = new HashMap<>();
+
+        int left = 0, right = 0;
+        int res = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            while (window.get(c) > 1) {
+                char d = s.charAt(left);
+                left++;
+                window.put(d, window.get(d) - 1);
+            }
+
+            res = Math.max(res, right - left);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        SlidingWindow2 foo = new SlidingWindow2();
+        System.out.println(foo.lengthOfLongestSubstring("pww"));
+    }
+}
