7174b25b1518846bc996fcc784dac107f8f344b3
Michael Qiu <michael.yiming.qiu@gmail.com>
2021-07-26 22:59:18 +1000

    116

---
+++ src/Algorithm/LeetCode/LeetCode_116_Populating_Next_Right_Pointers_in_Each_Node/Solution.java
@@ -0,0 +1,47 @@
+package Algorithm.LeetCode.LeetCode_116_Populating_Next_Right_Pointers_in_Each_Node;
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+public class Solution {
+    // 主函数
+    Node connect(Node root) {
+        if (root == null) return null;
+        connectTwoNode(root.left, root.right);
+        return root;
+    }
+
+    // 辅助函数
+    void connectTwoNode(Node node1, Node node2) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        /**** 前序遍历位置 ****/
+        // 将传入的两个节点连接
+        node1.next = node2;
+
+        // 连接相同父节点的两个子节点
+        connectTwoNode(node1.left, node1.right);
+        connectTwoNode(node2.left, node2.right);
+        // 连接跨越父节点的两个子节点
+        connectTwoNode(node1.right, node2.left);
+    }
+}
