d0b111496fef61bbd250e9866fdee6f437954409
Michael Qiu <michael.yiming.qiu@gmail.com>
2021-07-30 14:28:41 +1000

    230 652

---
+++ src/Algorithm/LeetCode/LeetCode_652_Find_Duplicate_Subtrees/Solution.java
@@ -0,0 +1,56 @@
+package Algorithm.LeetCode.LeetCode_652_Find_Duplicate_Subtrees;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    private final Map<String, Integer> memo = new HashMap<>();
+    private final List<TreeNode> res = new ArrayList<>();
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        traverse(root);
+
+        return res;
+    }
+
+    String traverse(TreeNode root) {
+        if (root == null) {
+            return "#";
+        }
+
+        String left = traverse(root.left);
+        String right = traverse(root.right);
+
+        String subTree = left + "," + right + "," + root.val;
+
+        int freq = memo.getOrDefault(subTree, 0);
+        // 多次重复也只会被加入结果集一次
+        if (freq == 1) {
+            res.add(root);
+        }
+        // 给子树对应的出现次数加一
+        memo.put(subTree, freq + 1);
+        return subTree;
+    }
+}
