b7cda313d7d895e5f896f084552ae193607a74ab
Michael Qiu <michael.yiming.qiu@gmail.com>
2021-08-22 14:54:27 +1000

    366 98

---
+++ src/Algorithm/LeetCode/LeetCode_366_Find_Leaves_of_Binary_Tree/Solution.java
@@ -0,0 +1,63 @@
+package Algorithm.LeetCode.LeetCode_366_Find_Leaves_of_Binary_Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        height(root, res);
+
+        return res;
+    }
+
+    private int height(TreeNode root, List<List<Integer>> res) {
+        if (root == null) {
+            return -1;
+        }
+
+        int level = 1 + Math.max(height(root.left, res), height(root.right, res));
+
+        if (res.size() < level + 1) {
+            res.add(new ArrayList<>());
+        }
+
+        res.get(level).add(root.val);
+
+        return level;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        TreeNode root;
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        solution.findLeaves(root);
+    }
+}
---
+++ src/Algorithm/LeetCode/LeetCode_98_Validate_Binary_Search_Tree/Solution_InOrder_Recursion_MyOwn.java
@@ -0,0 +1,48 @@
+package Algorithm.LeetCode.LeetCode_98_Validate_Binary_Search_Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_InOrder_Recursion_MyOwn {
+    public boolean isValidBST(TreeNode root) {
+        List<TreeNode> list = new ArrayList<>();
+
+        dfs(root,list);
+
+        for(int i = 1; i < list.size(); i++) {
+            if (list.get(i).val <= list.get(i - 1).val) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void dfs(TreeNode root, List<TreeNode> list) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.left != null) {
+            dfs(root.left, list);
+        }
+
+        list.add(root);
+
+        if (root.right != null) {
+            dfs(root.right, list);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution_InOrder_Recursion_MyOwn solution_inOrder_recursion = new Solution_InOrder_Recursion_MyOwn();
+
+        TreeNode root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        System.out.println(solution_inOrder_recursion.isValidBST(root));
+
+        root = new TreeNode(5, new TreeNode(4, new TreeNode(1), new TreeNode(7)), new TreeNode(8));
+        System.out.println(solution_inOrder_recursion.isValidBST(root));
+
+    }
+}
+
