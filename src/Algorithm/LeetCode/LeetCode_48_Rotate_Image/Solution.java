ec2f0ff896b329e140913f80ee5444a9bdbb61f7
Michael Qiu <michael.yiming.qiu@gmail.com>
2021-09-12 22:04:41 +1000

    48

---
+++ src/Algorithm/LeetCode/LeetCode_48_Rotate_Image/Solution.java
@@ -0,0 +1,33 @@
+package Algorithm.LeetCode.LeetCode_48_Rotate_Image;
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null && matrix.length == 0) {
+            return;
+        }
+
+        int n = matrix.length;
+
+        int top = 0;
+        int right = n - 1;
+        int bottom = n - 1;
+        int left = 0;
+
+        while (n > 1) {
+            for (int i = 0; i < n - 1; i++) {
+                int temp = matrix[top][left + i];
+
+                matrix[top][left + i] = matrix[bottom - i][left];
+                matrix[bottom - i][left] = matrix[bottom][right - i];
+                matrix[bottom][right - i] = matrix[top + i][right];
+                matrix[top + i][right] = temp;
+            }
+
+            n -= 2;
+            top++;
+            right--;
+            bottom--;
+            left++;
+        }
+    }
+}
