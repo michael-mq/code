5db74d9cec4d30707f77e89118306f6687005106
michael-mq <michael.yiming.qiu@gmail.com>
2022-05-26 00:53:36 +1000

    1249

---
+++ src/Algorithm/LeetCode/LeetCode_1249_Minimum_Remove_to_Make_Valid_Parentheses/Solution.java
@@ -0,0 +1,37 @@
+package Algorithm.LeetCode.LeetCode_1249_Minimum_Remove_to_Make_Valid_Parentheses;
+
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        int open = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                open++;
+            } else if (c == ')') {
+                if (open == 0) {
+                    continue;
+                }
+
+                open--;
+            }
+
+            sb.append(c);
+        }
+
+        if (open == 0) {
+            return sb.toString();
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = sb.length() - 1; i >= 0; i--) {
+            if (sb.charAt(i) == '(' && open-- > 0) {
+                continue;
+            }
+            result.append(sb.charAt(i));
+        }
+
+        return result.reverse().toString();
+    }
+}
