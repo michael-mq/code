ff41515ee58b2aa140f314e43e9c9aa7dcc0461c
MQ <mq@canva.com>
2024-06-04 00:35:47 +1000

    1101

--- src/Algorithm/Others/UnionFind/UniFind.java
+++ src/Algorithm/Others/UnionFind/UniFind.java
@@ -4,16 +4,16 @@ public class UniFind {
     // 连通分量个数
     private int count;
     // 存储一棵树
-    private int[] parent;
+    private final int[] root;
     // 记录树的“重量”
-    private int[] size;
+    private final int[] size;
 
     public UniFind(int n) {
         this.count = n;
-        parent = new int[n];
+        root = new int[n];
         size = new int[n];
         for (int i = 0; i < n; i++) {
-            parent[i] = i;
+            root[i] = i;
             size[i] = 1;
         }
     }
@@ -26,32 +26,32 @@ public class UniFind {
 
         // 小树接到大树下面，较平衡
         if (size[rootP] > size[rootQ]) {
-            parent[rootQ] = rootP;
+            root[rootQ] = rootP;
             size[rootP] += size[rootQ];
         } else {
-            parent[rootP] = rootQ;
+            root[rootP] = rootQ;
             size[rootQ] += size[rootP];
         }
         count--;
     }
 
-    public boolean connected(int p, int q) {
+    public boolean isConnected(int p, int q) {
         int rootP = find(p);
         int rootQ = find(q);
         return rootP == rootQ;
     }
 
     private int find(int x) {
-        while (parent[x] != x) {
+        while (root[x] != x) {
             // 进行路径压缩
-            parent[x] = parent[parent[x]];
-            x = parent[x];
+            root[x] = root[root[x]];
+            x = root[x];
         }
 
         return x;
     }
 
-    public int count() {
+    public int getCount() {
         return count;
     }
 }
