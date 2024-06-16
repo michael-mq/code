2d9fbc70493c60e6b991db1e24c99d1357ae1426
MQ <mq@canva.com>
2024-06-16 17:59:03 +1000

    648

---
+++ src/Algorithm/LeetCode/LeetCode_648_Replace_Words/Solution_HashSet.java
@@ -0,0 +1,33 @@
+package Algorithm.LeetCode.LeetCode_648_Replace_Words;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//https://leetcode.com/problems/replace-words/editorial/#approach-1-hash-set
+class Solution_HashSet {
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        String[] wordArray = sentence.split(" ");
+        Set<String> dictSet = new HashSet<>(dictionary);
+
+        // Replace each word in sentence with the corresponding shortest root
+        for (int i = 0; i < wordArray.length; i++) {
+            wordArray[i] = shortestRoot(wordArray[i], dictSet);
+        }
+
+        return String.join(" ", wordArray);
+    }
+
+    private String shortestRoot(String word, Set<String> dictSet) {
+        // Find the shortest root of the word in the dictionary
+        for (int i = 1; i <= word.length(); i++) {
+            String root = word.substring(0, i);
+            if (dictSet.contains(root)) {
+                return root;
+            }
+        }
+        // There is not a corresponding root in the dictionary
+        return word;
+    }
+}
---
+++ src/Algorithm/LeetCode/LeetCode_648_Replace_Words/Solution_Trie.java
@@ -0,0 +1,71 @@
+package Algorithm.LeetCode.LeetCode_648_Replace_Words;
+
+import java.util.List;
+
+//https://leetcode.com/problems/replace-words/editorial/#approach-2-prefix-trie
+class Solution_Trie {
+    static class TrieNode {
+
+        boolean isEnd;
+        TrieNode[] children;
+
+        TrieNode() {
+            isEnd = false;
+            children = new TrieNode[26];
+        }
+    }
+
+    static class Trie {
+
+        private TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode current = root;
+            for (char c : word.toCharArray()) {
+                if (current.children[c - 'a'] == null) {
+                    current.children[c - 'a'] = new TrieNode();
+                }
+                current = current.children[c - 'a'];
+            }
+            current.isEnd = true;
+        }
+
+        // Find the shortest root of the word in the trie
+        public String shortestRoot(String word) {
+            TrieNode current = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (current.children[c - 'a'] == null) {
+                    // There is not a corresponding root in the trie
+                    return word;
+                }
+                current = current.children[c - 'a'];
+                if (current.isEnd) {
+                    return word.substring(0, i + 1);
+                }
+            }
+            // There is not a corresponding root in the trie
+            return word;
+        }
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        String wordArray[] = sentence.split(" ");
+
+        Trie dictTrie = new Trie();
+        for (String word : dictionary) {
+            dictTrie.insert(word);
+        }
+
+        // Replace each word in the sentence with the corresponding shortest root
+        for (int word = 0; word < wordArray.length; word++) {
+            wordArray[word] = dictTrie.shortestRoot(wordArray[word]);
+        }
+
+        return String.join(" ", wordArray);
+    }
+}
