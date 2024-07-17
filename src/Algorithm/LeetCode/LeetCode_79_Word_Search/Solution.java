42701b0e79c75e743d9185a25aee9afb311cd8f9
MQ <mq@canva.com>
2024-07-17 19:49:23 +1000

    79

--- src/Algorithm/LeetCode/LeetCode_79_Word_Search/Solution.java
+++ src/Algorithm/LeetCode/LeetCode_79_Word_Search/Solution.java
@@ -2,10 +2,15 @@ package Algorithm.LeetCode.LeetCode_79_Word_Search;
 
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (helper(board, i, j, word, 0)) {
-                    return true;
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, i, j, 0, word)) {
+                        return true;
+                    }
                 }
             }
         }
@@ -13,22 +18,34 @@ class Solution {
         return false;
     }
 
-    boolean helper(char[][] board, int i, int j, String word, int index) {
-        if (index == word.length())
-            return true;
+    private boolean dfs(char[][] board, int i, int j, int index, String word) {
+        int length = word.length();
 
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || word.charAt(index) != board[i][j])
-            return false;
+        if (index == length - 1) {
+            return true;
+        }
 
         char temp = board[i][j];
+        board[i][j] = '0';
 
-        board[i][j] = '*';
-        boolean found = helper(board, i, j - 1, word, index + 1)
-                || helper(board, i + 1, j, word, index + 1)
-                || helper(board, i, j + 1, word, index + 1)
-                || helper(board, i - 1, j, word, index + 1);
+        int m = board.length;
+        int n = board[0].length;
+
+        int[][] directions = new int[][]{{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
+
+        for (int[] d : directions) {
+            int x = i + d[0];
+            int y = j + d[1];
+
+            if (x >= 0 && x < m && y >= 0 && y < n && board[x][y] == word.charAt(index + 1)) {
+                if (dfs(board, x, y, index + 1, word)) {
+                    return true;
+                }
+            }
+        }
+
         board[i][j] = temp;
 
-        return found;
+        return false;
     }
 }
