2654f8df291382939a1522eb06a0da5efbd9290a
MQ <mq@canva.com>
2024-08-22 13:26:42 +1000

    256

---
+++ src/Algorithm/LeetCode/LeetCode_256_Paint_House/Solution.java
@@ -0,0 +1,14 @@
+package Algorithm.LeetCode.LeetCode_256_Paint_House;
+
+//https://www.youtube.com/watch?v=PCC6Tshwxbo&t=275s
+class Solution {
+    public int minCost(int[][] costs) {
+        for (int i = 1; i < costs.length; i++) {
+            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
+        }
+
+        return Math.min(costs[costs.length - 1][0], Math.min(costs[costs.length - 1][1], costs[costs.length - 1][2]));
+    }
+}
