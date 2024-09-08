fab0c982e210371a6da8fc2f12062eaa549d54b6
MQ <mq@canva.com>
2024-09-08 17:00:39 +1000

    10

--- src/Algorithm/LeetCode/LeetCode_10_Regular_Expression_Matching/DP.java
+++ src/Algorithm/LeetCode/LeetCode_10_Regular_Expression_Matching/DP.java
@@ -1,18 +1,16 @@
 package Algorithm.LeetCode.LeetCode_10_Regular_Expression_Matching;
 
+// https://leetcode.cn/problems/regular-expression-matching/solutions/296114/shou-hui-tu-jie-wo-tai-nan-liao-by-hyj8
 public class DP {
     public boolean isMatch(String s, String p) {
-        if (s == null || p == null) {
-            return false;
-        }
-
         boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
 
         dp[0][0] = true;
 
-        for (int i = 1; i <= p.length(); i++) {
-            if (p.charAt(i - 1) == '*') {
-                dp[0][i] = dp[0][i - 2];
+        // the first character of p can't be '*'
+        for (int j = 2; j <= p.length(); j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
             }
         }
 
@@ -22,7 +20,9 @@ public class DP {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else if (p.charAt(j - 1) == '*') {
                     if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
-                        dp[i][j] = dp[i - 1][j] || dp[i][j - 2];
+                        dp[i][j] = dp[i][j - 2] ||
+                                dp[i - 1][j - 2] ||
+                                dp[i - 1][j];
                     } else {
                         dp[i][j] = dp[i][j - 2];
                     }
