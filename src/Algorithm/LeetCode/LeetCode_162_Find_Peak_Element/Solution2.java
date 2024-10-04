6a70c723522ca3558fdf2d282e76a17fd6e8524e
MQ <mq@canva.com>
2024-10-04 13:38:08 +1000

    162

--- src/Algorithm/LeetCode/LeetCode_162_Find_Peak_Element/Solution2.java
+++ src/Algorithm/LeetCode/LeetCode_162_Find_Peak_Element/Solution2.java
@@ -1,19 +1,21 @@
 package Algorithm.LeetCode.LeetCode_162_Find_Peak_Element;
 
+// https://youtu.be/IkQE4aN4zi0?t=2957
+// https://leetcode.cn/problems/find-peak-element/solutions/998441/gong-shui-san-xie-noxiang-xin-ke-xue-xi-qva7v
 public class Solution2 {
     public int findPeakElement(int[] nums) {
         int l = 0, r = nums.length - 1;
 
-        while (l < r) {
+        while (l + 1 < r) {
             int mid = l + (r - l) / 2;
 
-            if (nums[mid] > nums[mid + 1]) {
-                r = mid;
+            if (nums[mid] > nums[mid - 1]) {
+                l = mid;
             } else {
-                l = mid + 1;
+                r = mid;
             }
         }
 
-        return l;
+        return nums[l] > nums[r] ? l : r;
     }
 }
