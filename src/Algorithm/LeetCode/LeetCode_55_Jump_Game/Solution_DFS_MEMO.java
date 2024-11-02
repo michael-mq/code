5afba175a7bfa2383e853a362ef74f3028091b43
MQ <mq@canva.com>
2024-11-02 22:51:29 +1100

    55

--- src/Algorithm/LeetCode/LeetCode_55_Jump_Game/Solution_DFS_MEMO.java
+++ src/Algorithm/LeetCode/LeetCode_55_Jump_Game/Solution_DFS_MEMO.java
@@ -11,23 +11,24 @@ class Solution_DFS_MEMO {
         return dfs(nums, 0, memo);
     }
 
-    private boolean dfs(int[] nums, int index, int[] memo) {
+    boolean dfs(int[] nums, int index, int[] memo) {
         if (memo[index] != -1) {
-            return memo[index] != 0;
+            return memo[index] == 1;
         }
 
-        if (index == nums.length - 1) {
+        if (nums[index] + index >= nums.length - 1) {
+            // memo[index] = 1; // This doesn't matter because the first true will make the code end, and no need to memo
             return true;
         }
 
-        for (int i = index + 1; i <= Math.min(index + nums[index], nums.length - 1); i++) {
+        for (int i = index + 1; i <= index + nums[index]; i++) {
             if (dfs(nums, i, memo)) {
-                memo[i] = 1;
+                // memo[i] = 1; // As above
                 return true;
             }
         }
 
-        memo[index] = 0;
+        memo[index] = 0; // This matters
         return false;
     }
 }
