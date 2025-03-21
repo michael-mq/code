1da7c35f7306c39f4356fe91605a6d5555649753
MQ <mq@canva.com>
2025-03-21 22:04:49 +1100

    43

--- src/Algorithm/LeetCode/LeetCode_43_Multiply_Strings/Solution.java
+++ src/Algorithm/LeetCode/LeetCode_43_Multiply_Strings/Solution.java
@@ -1,40 +1,30 @@
 package Algorithm.LeetCode.LeetCode_43_Multiply_Strings;
 
+//https://leetcode.cn/problems/multiply-strings/solutions/372098/zi-fu-chuan-xiang-cheng-by-leetcode-solution
 class Solution {
     public String multiply(String num1, String num2) {
         if (num1.equals("0") || num2.equals("0")) {
             return "0";
         }
-
-        int len1 = num1.length();
-        int len2 = num2.length();
-        int[] res = new int[len1 + len2];
-
-        for (int i = len1 - 1; i >= 0; i--) {
-            int n1 = num1.charAt(i) - '0';
-
-            for (int j = len2 - 1; j >= 0 ; j--) {
-                int n2 = num2.charAt(j) - '0';
-
-                int pos1 = i + j;
-                int pos2 = i + j + 1;
-
-                int sum = n1 * n2 + res[pos1] * 10 + res[pos2];
-                res[pos1] = sum / 10;
-                res[pos2] = sum % 10;
+        int m = num1.length(), n = num2.length();
+        int[] ansArr = new int[m + n];
+        for (int i = m - 1; i >= 0; i--) {
+            int x = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j--) {
+                int y = num2.charAt(j) - '0';
+                ansArr[i + j + 1] += x * y;
             }
         }
-
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < res.length; i++) {
-            if (i == 0 && res[i] == 0) {
-                continue;
-            }
-
-            result.append(res[i]);
+        for (int i = m + n - 1; i > 0; i--) {
+            ansArr[i - 1] += ansArr[i] / 10;
+            ansArr[i] %= 10;
         }
-
-        return result.toString();
+        int index = ansArr[0] == 0 ? 1 : 0;
+        StringBuffer ans = new StringBuffer();
+        while (index < m + n) {
+            ans.append(ansArr[index]);
+            index++;
+        }
+        return ans.toString();
     }
 }
