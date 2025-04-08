011c976bc3f85659e9c5a94a526aaf720ac475c8
MQ <mq@canva.com>
2025-04-08 20:32:50 +1000

    809

---
+++ src/Algorithm/LeetCode/LeetCode_809_Expressive_Words/Solution_MyOwn_ReWriting.java
@@ -0,0 +1,64 @@
+package Algorithm.LeetCode.LeetCode_809_Expressive_Words;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution_MyOwn_ReWriting {
+    public int expressiveWords(String s, String[] words) {
+        rle R1 = new rle(s);
+        List<Integer> c1 = R1.counts;
+        int ans = 0;
+
+        search:
+        for (String w : words) {
+            rle R2 = new rle(w);
+            List<Integer> c2 = R2.counts;
+
+            if (!R1.key.equals(R2.key)) {
+                continue;
+            }
+
+            for (int i = 0; i < c1.size(); i++) {
+                if (c1.get(i) != c2.get(i) && c1.get(i) < 3 || c1.get(i) < c2.get(i)) {
+                    continue search;
+                }
+            }
+
+            ans++;
+        }
+
+        return ans;
+    }
+}
+
+class rle {
+    String key = "";
+    List<Integer> counts = new ArrayList<>();
+
+    public rle(String S) {
+        StringBuilder sb = new StringBuilder();
+
+        int count = 1;
+        char c = S.charAt(0);
+
+        for (int i = 1; i <= S.length(); i++) {
+            if (i == S.length()) {
+                sb.append(c);
+                counts.add(count);
+                break;
+            }
+
+            if (S.charAt(i) != S.charAt(i - 1)) {
+                sb.append(c);
+                counts.add(count);
+                count = 1;
+                c = S.charAt(i);
+                continue;
+            }
+
+            count++;
+        }
+
+        key = sb.toString();
+    }
+}
