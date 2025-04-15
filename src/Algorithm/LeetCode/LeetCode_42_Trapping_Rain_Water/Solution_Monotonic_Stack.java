cc427ab0a5bb81d458031ff054f2b5627bbad0f7
MQ <mq@canva.com>
2025-04-15 17:40:21 +1000

    42

--- src/Algorithm/LeetCode/LeetCode_42_Trapping_Rain_Water/Solution_Monotonic_Stack.java
+++ src/Algorithm/LeetCode/LeetCode_42_Trapping_Rain_Water/Solution_Monotonic_Stack.java
@@ -1,32 +1,28 @@
 package Algorithm.LeetCode.LeetCode_42_Trapping_Rain_Water;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
 
 // https://leetcode.cn/problems/trapping-rain-water/solutions/692342/jie-yu-shui-by-leetcode-solution-tuvc/ Solution 2
 class Solution_Monotonic_Stack {
     public int trap(int[] height) {
-        int n = height.length;
-        Stack<Integer> stack = new Stack<>();
         int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && height[stack.peek()] < height[i]) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        int n = height.length;
+        for (int i = 0; i < n; ++i) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
                 int top = stack.pop();
-
                 if (stack.isEmpty()) {
                     break;
                 }
-
                 int left = stack.peek();
                 int currWidth = i - left - 1;
                 int currHeight = Math.min(height[left], height[i]) - height[top];
-
-                ans = currWidth * currHeight;
+                ans += currWidth * currHeight;
             }
-
             stack.push(i);
         }
-
         return ans;
     }
 }
