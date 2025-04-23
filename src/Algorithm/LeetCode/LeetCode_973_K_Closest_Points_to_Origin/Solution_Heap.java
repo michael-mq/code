e3a7ec53e641acdf877174d5b36eaf85b8546c82
MQ <mq@canva.com>
2025-04-23 12:52:46 +1000

    973

--- src/Algorithm/LeetCode/LeetCode_973_K_Closest_Points_to_Origin/Solution_Heap.java
+++ src/Algorithm/LeetCode/LeetCode_973_K_Closest_Points_to_Origin/Solution_Heap.java
@@ -1,28 +1,28 @@
 package Algorithm.LeetCode.LeetCode_973_K_Closest_Points_to_Origin;
 
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Solution_Heap {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<double[]> queue = new PriorityQueue<>((a, b) -> (int) (b[1] - a[1]));
+        Queue<double[]> maxHeap = new PriorityQueue<>((a, b) -> (int) (b[1] - a[1]));
 
         for (int i = 0; i < points.length; i++) {
             double distance = Math.pow(points[i][0], 2) + Math.pow(points[i][1], 2);
-
             double[] point = new double[]{i, distance};
-            queue.offer(point);
 
-            if (queue.size() > k) {
-                queue.poll();
+            maxHeap.offer(point);
+
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
             }
         }
 
         int[][] res = new int[k][];
-        int index = 0;
 
-        while (!queue.isEmpty()) {
-            int n = (int) queue.peek()[0];
-            res[index++] = points[n];
+        for (int i = 0; i < res.length; i++) {
+            int index = (int) maxHeap.poll()[0];
+            res[i] = points[index];
         }
 
         return res;
