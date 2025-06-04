d0fee6adc04c9617fbe65a9f0aa9ac9dcf4961e5
MQ <mq@canva.com>
2025-06-04 19:50:26 +1000

    121

--- src/Algorithm/LeetCode/LeetCode_121_Best_Time_to_Buy_and_Sell_Stock/Solution_DP.java
+++ src/Algorithm/LeetCode/LeetCode_121_Best_Time_to_Buy_and_Sell_Stock/Solution_DP.java
@@ -9,15 +9,17 @@ public class Solution_DP {
         // dp[i][0]代表第i天持有股票的最大收益
         // dp[i][1]代表第i天不持有股票的最大收益
         int[][] dp = new int[length][2];
-        int result = 0;
+
         dp[0][0] = -prices[0];
         dp[0][1] = 0;
+
         for (int i = 1; i < length; i++) {
             // 继续持有 或者 买了(重点：所有的天数里面，就只能买买卖一次，也是第一次，that's why - prices[i])
             dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
             // 卖了 或者 继续不持有
             dp[i][1] = Math.max(dp[i - 1][0] + prices[i], dp[i - 1][1]);
         }
+
         return dp[length - 1][1];
     }
 }
