d23d7c06fb3113c0438e7b10267dc3c194d08c7f
MQ <mq@canva.com>
2025-06-14 11:51:17 +1000

    322

--- src/Algorithm/LeetCode/LeetCode_322_Coin_Change/Solution_DP2.java
+++ src/Algorithm/LeetCode/LeetCode_322_Coin_Change/Solution_DP2.java
@@ -6,18 +6,18 @@ import java.util.Arrays;
 public class Solution_DP2 {
     public int coinChange(int[] coins, int amount) {
         // 自底向上的动态规划
-        if(coins.length == 0){
+        if (coins.length == 0) {
             return -1;
         }
 
         // memo[n]的值： 表示的凑成总金额为n所需的最少的硬币个数
-        int[] memo = new int[amount+1];
+        int[] memo = new int[amount + 1];
         memo[0] = 0;
-        for(int i = 1; i <= amount;i++){
+        for (int i = 1; i <= amount; i++) {
             int min = Integer.MAX_VALUE;
-            for(int j = 0;j < coins.length;j++){
-                if(i - coins[j] >= 0 && memo[i-coins[j]] < min){
-                    min = memo[i-coins[j]] + 1;
+            for (int coin : coins) {
+                if (i - coin >= 0 && memo[i - coin] < min) {
+                    min = memo[i - coin] + 1;
                 }
             }
             // memo[i] = (min == Integer.MAX_VALUE ? Integer.MAX_VALUE : min);
