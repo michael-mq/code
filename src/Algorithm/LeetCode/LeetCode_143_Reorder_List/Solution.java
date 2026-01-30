a97c1857cba88fd141d04292415f0a8e094a78fe
MQ <mq@canva.com>
2026-01-30 16:53:41 +1100

    143

--- src/Algorithm/LeetCode/LeetCode_143_Reorder_List/Solution.java
+++ src/Algorithm/LeetCode/LeetCode_143_Reorder_List/Solution.java
@@ -1,53 +1,41 @@
 package Algorithm.LeetCode.LeetCode_143_Reorder_List;
 
-public class Solution {
+//https://leetcode.com/problems/reorder-list/editorial
+class Solution {
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null || head.next.next == null) {
-            return;
-        }
+        if (head == null) return;
 
-        //找中点，链表分成两个
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast.next != null && fast.next.next != null) {
+        // find the middle of linked list [Problem 876]
+        // in 1->2->3->4->5->6 find 4
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        ListNode newHead = slow.next;
-        slow.next = null;
+        // reverse the second part of the list [Problem 206]
+        // convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
+        // reverse the second half in-place
+        ListNode prev = null, curr = slow, tmp;
+        while (curr != null) {
+            tmp = curr.next;
 
-        //第二个链表倒置
-        newHead = reverseList(newHead);
-
-        //链表节点依次连接
-        while (newHead != null) {
-            ListNode temp = newHead.next;
-            newHead.next = head.next;
-            head.next = newHead;
-            head = newHead.next;
-            newHead = temp;
-        }
-    }
-
-    private ListNode reverseList(ListNode head) {
-        if (head == null) {
-            return null;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
         }
 
-        ListNode tail = head;
-        head = head.next;
+        // merge two sorted linked lists [Problem 21]
+        // merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
+        ListNode first = head, second = prev;
+        while (second.next != null) {
+            tmp = first.next;
+            first.next = second;
+            first = tmp;
 
-        tail.next = null;
-
-        while (head != null) {
-            ListNode temp = head.next;
-            head.next = tail;
-            tail = head;
-            head = temp;
+            tmp = second.next;
+            second.next = first;
+            second = tmp;
         }
-
-        return tail;
     }
 }
