88a771e4fa54a14a0c3f1140189e9ac2012cc8e0
MQ <mq@canva.com>
2026-04-29 17:26:47 +1000

    283

--- src/Algorithm/LeetCode/LeetCode_283_Move_Zeroes/Solution.java
+++ src/Algorithm/LeetCode/LeetCode_283_Move_Zeroes/Solution.java
@@ -9,17 +9,25 @@ public class Solution {
             return;
         }
 
-        //第一次遍历的时候，j指针记录非0的个数，只要是非0的统统都赋给nums[j]
         int j = 0;
+        // 第一次遍历：将非零元素挪到前面
         for (int i = 0; i < nums.length; ++i) {
             if (nums[i] != 0) {
-                nums[j++] = nums[i];
+                // 优化点：只有当 i 和 j 不相等时才赋值
+                // 如果相等，说明还没遇到 0，元素本来就在正确的位置上
+                if (i != j) {
+                    nums[j] = nums[i];
+                }
+                j++;
             }
         }
-        //非0元素统计完了，剩下的都是0了
-        //所以第二次遍历把末尾的元素都赋为0即可
+
+        // 第二次遍历：将剩余位置补零
         for (int i = j; i < nums.length; ++i) {
-            nums[i] = 0;
+            // 优化点：如果当前位置已经是 0 了，就没必要再写一遍 0
+            if (nums[i] != 0) {
+                nums[i] = 0;
+            }
         }
     }
 
